a1476aaedb21d8d08bda90a84d4695a958569a3b
Ivan Robles <irae45@gmail.com>
2018-02-05 13:33:04 -0600

    Agregamos los datos normalizados al Store y renderizamos en las vistas la data normalizada

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home'
-import data from '../api.json';
-import normalizedData from '../schemas/index';
+import data from '../schemas/index';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers/data';
 import { normalize } from 'normalizr';
 
-console.log(normalizedData);
-
 const initialState = {
   data: {
-    ...data,
+    entities: data.entities,
+    categories: data.result.categories,
   },
   search: [],
 };
@@ -22,8 +20,6 @@ const store = createStore(
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-
-console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container');
 
