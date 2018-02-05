f0fd1c7da8b84784af65e370949b6fbdeffb7714
Ivan Robles <irae45@gmail.com>
2018-02-05 12:36:09 -0600

    Normalizamos los esquemas de la data con Normalizr

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home'
 import data from '../api.json';
+import normalizedData from '../schemas/index';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers/data';
+import { normalize } from 'normalizr';
+
+console.log(normalizedData);
 
 const initialState = {
   data: {
