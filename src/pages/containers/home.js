251103f447e521e0d936a4c0de2d8ab8680122a3
Ivan Robles <irae45@gmail.com>
2018-02-09 17:43:03 -0600

    Enlazamos los actions creators con bindActionCreators de redux y mapDispatchToProps

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -9,14 +9,16 @@ import VideoPlayer from '../../player/containers/video-player';
 import { connect } from 'react-redux';
 import { List as list } from 'immutable';
 import { openModal, closeModal} from '../../actions/index';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions/index';
 
 class Home extends Component {
 
   handleOpenModal = (id) => {
-    this.props.dispatch(openModal(id));
+    this.props.actions.openModal(id);
   }
   handleCloseModal = (event) => {
-    this.props.dispatch(closeModal());
+    this.props.actions.closeModal();
   }
 
   render() {
@@ -66,4 +68,10 @@ function mapStateToProps(state, props) {
   }
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
