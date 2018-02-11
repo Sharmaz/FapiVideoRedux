669b5b7d81dc111255e3b86f56eb98461e35aea4
Ivan Robles <irae45@gmail.com>
2018-02-11 13:23:47 -0600

    Aplicamos un middleware (ejemplo de la documentacion de redux) para mostar logs del estado de la app

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home'
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers/index';
 import { Map as map } from 'immutable';
 import { normalize } from 'normalizr';
 
+const logger = ({ getState, dispatch }) => next => action => {
+  console.log('Estado Previo', getState().toJS());
+  console.log('Acción', action);
+  const value = next(action);
+  console.log('Nuevo Estado', getState().toJS());
+  return value;
+}
+
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 const homeContainer = document.getElementById('home-container');
